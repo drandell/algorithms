8b7f70d89e89341fdc7cbdd69a51984e5e4ac8df
drandell <danielrandell@hotmail.com>
2013-12-10 18:07:54 +0000

    Updated Algo

--- Algorithms.java
+++ Algorithms.java
@@ -1,15 +1,44 @@
+import java.util.Arrays;
+
 public class Algorithms {
+	public static int getMaxValue(int[] array) {
+		int maxValue = array[0];
+
+		for (int i = 1; i < array.length; i++) {
+		    if (array[i] > maxValue) {
+		    	maxValue = array[i];
+		    }
+		} 
+		return maxValue;
+	}
+	
+	public static int getMinValue(int[] array) {  
+	     int minValue = array[0]; 
+	     
+	     for(int i = 1; i < array.length; i++) {  
+	    	 if(array[i] < minValue) {  
+	    		 minValue = array[i];  
+	    	 }  
+	     }  
+	    return minValue;  
+	} 
+	
 	//Source@ http://rosettacode.org/wiki/Sorting_algorithms/Counting_sort#Java
 	public static void countingSort(int[] array, int min, int max) {
+		//Create Counter Array
 		int[] count= new int[max - min + 1];
 		for(int number : array) {
+			//Increment counter array of counts 
 			count[number - min]++;
 		}
 		int z= 0;
 		for(int i= min;i <= max;i++){
+			//Loop through array of counters starting at min
 			while(count[i - min] > 0){
+				//Place sorted i into array at z
 				array[z]= i;
 				z++;
+				//decrement counter after being sorted
 				count[i - min]--;
 			}
 		}
@@ -28,6 +57,12 @@ public class Algorithms {
 			}
 			//"value" can no longer move
 			A[j + 1] = value;
+			
+			//Sample Data - Half way results!
+			//if (i == A.length / 2) {
+			//	System.out.println("Intermediary sort: " + Arrays.toString(A));
+			//}
+			
 		}
 	}
 }
